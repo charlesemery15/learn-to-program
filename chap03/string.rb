5f249472bc229c2b50e2a8b656dcadc5e9711ee4
charles <31915103+charlesemery15@users.noreply.github.com>
2017-10-31 17:30:04 +0000

    chap03

---
+++ chap03/string.rb
@@ -0,0 +1,11 @@
+puts "Hello World!"
+puts "I like" + "apple pie"
+puts "I like " + "apple pie"
+puts "blink " * 4
+puts 5 + 5
+puts "5 + 5"
+#puts "5" + 5
+#backslash
+#puts 'you're a mug
+puts "you're a mug"
+puts 'you\'re a mug'
