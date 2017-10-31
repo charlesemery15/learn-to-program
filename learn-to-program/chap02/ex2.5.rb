e57280421a6b43240b8e876836f32646c548e4f5
charles <31915103+charlesemery15@users.noreply.github.com>
2017-10-31 16:14:49 +0000

    Add files via upload

---
+++ learn-to-program/chap02/ex2.5.rb
@@ -0,0 +1,8 @@
+#hours in a year
+puts 1 * 365 * 24
+#mins in a decade
+puts 10 * 365 * 24 * 60
+#age in seconds
+puts (25 * 365 * 24 * 60 * 60) + ((15 + 30) * 24 * 60 * 60)
+#age of author
+puts 1160000000 / (60 * 60 * 24 * 365)
